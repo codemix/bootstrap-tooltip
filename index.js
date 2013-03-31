79b72fe9d57a487a9b71e4911ade6d42673d83f2
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:33 +0100

    Added tag v2.1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ===========================================================
-   * bootstrap-tooltip.js v2.0.4
+   * bootstrap-tooltip.js v2.1.0
    * http://twitter.github.com/bootstrap/javascript.html#tooltips
    * Inspired by the original jQuery.tipsy by Jason Frame
    * ===========================================================
@@ -46,11 +46,13 @@ function(){
         this.options = this.getOptions(options)
         this.enabled = true
   
-        if (this.options.trigger != 'manual') {
-          eventIn  = this.options.trigger == 'hover' ? 'mouseenter' : 'focus'
+        if (this.options.trigger == 'click') {
+          this.$element.on('click.' + this.type, this.options.selector, $.proxy(this.toggle, this))
+        } else if (this.options.trigger != 'manual') {
+          eventIn = this.options.trigger == 'hover' ? 'mouseenter' : 'focus'
           eventOut = this.options.trigger == 'hover' ? 'mouseleave' : 'blur'
-          this.$element.on(eventIn, this.options.selector, $.proxy(this.enter, this))
-          this.$element.on(eventOut, this.options.selector, $.proxy(this.leave, this))
+          this.$element.on(eventIn + '.' + this.type, this.options.selector, $.proxy(this.enter, this))
+          this.$element.on(eventOut + '.' + this.type, this.options.selector, $.proxy(this.leave, this))
         }
   
         this.options.selector ?
@@ -150,20 +152,11 @@ function(){
         }
       }
   
-    , isHTML: function(text) {
-        // html string detection logic adapted from jQuery
-        return typeof text != 'string'
-          || ( text.charAt(0) === "<"
-            && text.charAt( text.length - 1 ) === ">"
-            && text.length >= 3
-          ) || /^(?:[^<]*<[\w\W]+>[^>]*$)/.exec(text)
-      }
-  
     , setContent: function () {
         var $tip = this.tip()
           , title = this.getTitle()
   
-        $tip.find('.tooltip-inner')[this.isHTML(title) ? 'html' : 'text'](title)
+        $tip.find('.tooltip-inner')[this.options.html ? 'html' : 'text'](title)
         $tip.removeClass('fade in top bottom left right')
       }
   
@@ -187,6 +180,8 @@ function(){
         $.support.transition && this.$tip.hasClass('fade') ?
           removeWithAnimation() :
           $tip.remove()
+  
+        return this
       }
   
     , fixTitle: function () {
@@ -246,6 +241,10 @@ function(){
         this[this.tip().hasClass('in') ? 'hide' : 'show']()
       }
   
+    , destroy: function () {
+        this.hide().$element.off('.' + this.type).removeData(this.type)
+      }
+  
     }
   
   
@@ -272,6 +271,7 @@ function(){
     , trigger: 'hover'
     , title: ''
     , delay: 0
+    , html: true
     }
   
   }(jQuery);
