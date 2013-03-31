d1a941cb369a03159dfd21e4b705e7bfc3b75d1f
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:35 +0100

    Added tag v2.1.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* ===========================================================
-   * bootstrap-tooltip.js v2.1.0
+   * bootstrap-tooltip.js v2.1.1
    * http://twitter.github.com/bootstrap/javascript.html#tooltips
    * Inspired by the original jQuery.tipsy by Jason Frame
    * ===========================================================
